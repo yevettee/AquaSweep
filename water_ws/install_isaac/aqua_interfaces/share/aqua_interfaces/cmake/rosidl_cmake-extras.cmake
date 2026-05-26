7dec9985d2a4a19c87e91f55b6c9877f47f72062
sunga4017 <sunga4017@gmail.com>
2026-05-26 17:11:59 +0900

    chore: remove build and install artifacts from git tracking

--- water_ws/install_isaac/aqua_interfaces/share/aqua_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(aqua_interfaces_IDL_FILES "msg/RobotStatus.idl;msg/TankStatus.idl;msg/TankPhysicalVariables.idl;action/CleanFloor.idl;action/CleanWall.idl")
-set(aqua_interfaces_INTERFACE_FILES "msg/RobotStatus.msg;msg/TankStatus.msg;msg/TankPhysicalVariables.msg;action/CleanFloor.action;action/CleanWall.action")
