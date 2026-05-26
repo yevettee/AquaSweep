d028e18d20a8d1cf9250fb3ca34043fa086afb14
sunga4017 <sunga4017@gmail.com>
2026-05-26 17:14:04 +0900

    chore: remove remaining build and log artifacts from git tracking

--- water_ws/build_isaac/aqua_interfaces/rosidl_generator_py/aqua_interfaces/action/__init__.py
+++
@@ -1,2 +0,0 @@
-from aqua_interfaces.action._clean_floor import CleanFloor  # noqa: F401
-from aqua_interfaces.action._clean_wall import CleanWall  # noqa: F401
--- water_ws/build_isaac/aqua_interfaces/rosidl_generator_py/aqua_interfaces/msg/__init__.py
+++
@@ -1,3 +0,0 @@
-from aqua_interfaces.msg._robot_status import RobotStatus  # noqa: F401
-from aqua_interfaces.msg._tank_physical_variables import TankPhysicalVariables  # noqa: F401
-from aqua_interfaces.msg._tank_status import TankStatus  # noqa: F401
